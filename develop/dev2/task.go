fb5185f9376f11e4160f67346b907bc7784eca52
vlad <vlad.gromov666@gmail.com>
2024-02-08 21:17:18 +0300

    2024.02.08 - refactoring dev2

--- develop/dev2/task.go
+++ develop/dev2/task.go
@@ -9,50 +9,75 @@ import (
 	"unicode"
 )
 
+/*
+ErrorIncorrectString - ошибка невено введёной строки
+*/
 var ErrorIncorrectString = errors.New("incorrect string")
 
-func addRns(strBldr *strings.Builder, run rune, count int) {
+/*
+addRns - функция для добавления рун в результирующую строку
+strBldr *strings.Builder - указатель на результирующую строку
+rn rune - руна которую нужно записать в строку
+count int - количество раз, сколько нужно добавить руну в строку
+*/
+func addRns(strBldr *strings.Builder, rn rune, count int) {
 	for j := 1; j < count; j++ {
-		strBldr.WriteString(string(run))
+		strBldr.WriteRune(rn)
 	}
 }
 
-func unpackString(str string) (string, error) {
+/*
+UnpackString - функция для распаковки строки
+str string - искомая строка
+*/
+func UnpackString(str string) (string, error) {
 	var res strings.Builder
 	rns := []rune(str)
 
 	for i := 0; i < len(rns); i++ {
+		// если первая руна в строке число, возвращаем ошибку
 		if i == 0 && unicode.IsDigit(rns[i]) {
 			return "", ErrorIncorrectString
 		}
+		// проверка на \ и что текущая руна число
 		if i > 1 && rns[i-2] == '\\' && unicode.IsDigit(rns[i]) {
+			// если встретилось \\ записываем предыдущий \ n раз
 			if rns[i-1] == '\\' {
+				fmt.Println(rns[i] - '0')
 				addRns(&res, rns[i-1], int(rns[i]-'0'))
 				continue
 			}
+			// если предудущая руна число и до него есть \ и текущая руна число, выводим предыдущее число n раз
 			if unicode.IsDigit(rns[i-1]) {
 				addRns(&res, rns[i-1], int(rns[i]-'0'))
 				continue
 			}
 		}
+
+		// если текущая руна \ и предыдущие условия не выполнились
 		if rns[i] == '\\' {
+			// если это не первый элемент и предыдущий равен \ то просто выводим \
 			if i > 0 && rns[i-1] == '\\' {
 				res.WriteString(string(rns[i]))
 				continue
 			}
 			continue
 		}
+		// если текущая руна число и предудущие условия не выполнились
 		if unicode.IsDigit(rns[i]) {
+			// если предвдущая руна \ то просто выводим число
 			if rns[i-1] == '\\' {
 				res.WriteString(string(rns[i]))
 				continue
 			}
+			// если предыдущая руна тоже число, значит строка некорректна иначе выводим предыдущую руну n раз
 			if unicode.IsDigit(rns[i-1]) {
 				return "", ErrorIncorrectString
 			} else {
 				addRns(&res, rns[i-1], int(rns[i]-'0'))
 				continue
 			}
+			// иначе просто записываем руну в результат
 		} else {
 			res.WriteString(string(rns[i]))
 			continue
@@ -72,7 +97,7 @@ func main() {
 		return
 	}
 
-	umpck, err := unpackString(text)
+	umpck, err := UnpackString(text)
 
 	if err != nil {
 		fmt.Println(err)
--- develop/dev2/task_test.go
+++ develop/dev2/task_test.go
@@ -1,39 +1,108 @@
 package main
 
-import "testing"
-
-func passedString(t *testing.T, str string, correctStr string) {
-	text, err := unpackString(str)
-	if err != nil {
-		t.Errorf("incorrect error return by string: %s", str)
-	}
-	if text != correctStr {
-		t.Errorf("incorrect result (expected %s)", correctStr)
-	}
-}
-
-func incorrectString(t *testing.T, str string) {
-	text, err := unpackString(str)
-	if err == nil {
-		t.Errorf("incorrect error return by string: %s, expected error", str)
-	}
-	if text != "" {
-		t.Error("incorrect result, expected empty string")
-	}
-}
+import (
+	"errors"
+	"testing"
+)
 
 func TestUnpacking(t *testing.T) {
-	passedString(t, "a4bc2d5e", "aaaabccddddde")
-	passedString(t, "abcd", "abcd")
-	passedString(t, "-5", "-----")
-	passedString(t, "", "")
-	passedString(t, "qwe\\4\\5", "qwe45")
-	passedString(t, "qwe\\45", "qwe44444")
-	passedString(t, "qwe\\\\5", "qwe\\\\\\\\\\")
-	passedString(t, "\\", "")
-	passedString(t, "\\\\", "\\")
-	passedString(t, "\\4", "4")
-	incorrectString(t, "45")
-	incorrectString(t, "5")
-	incorrectString(t, "a45")
+	tsts := []struct {
+		name    string
+		arg     string
+		want    string
+		wantErr error
+	}{
+		{
+			name:    "number after letter",
+			arg:     "a4bc2d5e",
+			want:    "aaaabccddddde",
+			wantErr: nil,
+		},
+		{
+			name:    "string without \\, \\\\ and numbers",
+			arg:     "abcd",
+			want:    "abcd",
+			wantErr: nil,
+		},
+		{
+			name:    "symbol - with number",
+			arg:     "-5",
+			want:    "-----",
+			wantErr: nil,
+		},
+		{
+			name:    "numbers in string with \\",
+			arg:     "qwe\\4\\5",
+			want:    "qwe45",
+			wantErr: nil,
+		},
+		{
+			name:    "number after number with \\",
+			arg:     "qwe\\45",
+			want:    "qwe44444",
+			wantErr: nil,
+		},
+		{
+			name:    "number after \\\\",
+			arg:     "qwe\\\\5",
+			want:    "qwe\\\\\\\\\\",
+			wantErr: nil,
+		},
+		{
+			name:    "just \\",
+			arg:     "\\",
+			want:    "",
+			wantErr: nil,
+		},
+		{
+			name:    "empty string",
+			arg:     "",
+			want:    "",
+			wantErr: nil,
+		},
+		{
+			name:    "just \\\\",
+			arg:     "\\\\",
+			want:    "\\",
+			wantErr: nil,
+		},
+		{
+			name:    "just number after \\",
+			arg:     "\\4",
+			want:    "4",
+			wantErr: nil,
+		},
+		{
+			name:    "incorrect string with first number",
+			arg:     "45asdf",
+			want:    "",
+			wantErr: ErrorIncorrectString,
+		},
+		{
+			name:    "incorrect string with only one number",
+			arg:     "5",
+			want:    "",
+			wantErr: ErrorIncorrectString,
+		},
+		{
+			name:    "incorrect string with double number after not number letter",
+			arg:     "a45",
+			want:    "",
+			wantErr: ErrorIncorrectString,
+		},
+	}
+
+	for _, tt := range tsts {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := UnpackString(tt.arg)
+
+			if !errors.Is(err, tt.wantErr) {
+				t.Errorf("UnpackString() error: %s, want err: %s", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("UnpackString() result: %s, want result: %s", got, tt.want)
+			}
+		})
+	}
 }
