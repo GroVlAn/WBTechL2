584d0ce6a124004515d8f8b1be895e030616749e
vlad <vlad.gromov666@gmail.com>
2024-02-08 23:30:58 +0300

    2024.02.08 - refactoring dev7

--- develop/dev7/task.go
+++ develop/dev7/task.go
@@ -6,22 +6,29 @@ import (
 	"time"
 )
 
+// merge - функция для слияния n каналов
 func merge(channels ...<-chan interface{}) <-chan interface{} {
 	var wg sync.WaitGroup
+	// новый канал, в который будут записаны значения
 	merged := make(chan interface{})
 
+	// создаём столько горутин, сколько у нас пришло каналов
 	for _, chIt := range channels {
+		// обновляем счётчик WaitGroup - количество горутин, чьё завершение нужно ожидать
 		wg.Add(1)
 
 		go func(ch <-chan interface{}) {
+			// в самом конце закрываем канал
 			defer wg.Done()
 			for val := range ch {
-				fmt.Println(val)
+				// записываем в результирующий канал значения
 				merged <- val
 			}
 		}(chIt)
 	}
 
+	// в отдельной горутине ожидаем выполнения всех горутин и закрываем результирующий канал
+	// для того, чтобы наша функция была не блокирующей
 	go func() {
 		wg.Wait()
 		close(merged)
